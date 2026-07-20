b1590fc07adaee20953d246516015a27c31f70ac
saajiidi <saajiidi@gmail.com>
2026-07-20 13:51:18 +0600

    .

--- src/pages/dashboard_output.py
+++ src/pages/dashboard_output.py
@@ -553,7 +553,13 @@ def render_dashboard_output(
 
     if is_operational:
         nav_mode = st.session_state.get("wc_nav_mode", "Today")
-        m_df = st.session_state.get("wc_curr_df" if nav_mode in ["Today", "Backlog"] else "wc_prev_df")
+        if nav_mode == "Today":
+            m_df = st.session_state.get("wc_curr_df")
+        elif nav_mode == "Backlog":
+            m_df = st.session_state.get("wc_backlog_df")
+        else:
+            m_df = st.session_state.get("wc_prev_df")
+            
         c_df = st.session_state.get("wc_prev_df" if nav_mode == "Today" else "wc_curr_df") if nav_mode != "Backlog" else None
         drill, summ, top, basket, active_df = _render_operational_cycle_metrics(m_df, c_df, nav_mode, dummy_mapping, wc_raw_mapping)
     else:
